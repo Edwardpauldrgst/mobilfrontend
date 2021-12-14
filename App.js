267890737b485fa0a7c37bde5ff74599966dd662
Edwardpauldrgst <dreagaster@gmail.com>
2021-12-14 08:42:11 +0100

    help

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import Ranglista from './Ranglista';
 
 function HomeScreen({ navigation }) {
   return (
@@ -16,9 +17,7 @@ function HomeScreen({ navigation }) {
 
 function NotificationsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
+    <Ranglista></Ranglista>
   );
 }
 
@@ -27,9 +26,9 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator initialRouteName="Főoldal">
+        <Drawer.Screen name="Főoldal" component={HomeScreen} />
+        <Drawer.Screen name="Ranglista" component={NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
